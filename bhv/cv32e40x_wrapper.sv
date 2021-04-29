1f4389e419650d23c70d90e53a19b63cddd550f4
Oivind Ekelund <oivind.ekelund@silabs.com>
2021-04-29 11:56:09 +0200

    Fix range of PMA_CFG parameter to avoid negative indexes
    Signed-off-by: Oivind Ekelund <oivind.ekelund@silabs.com>

--- bhv/cv32e40x_wrapper.sv
+++ bhv/cv32e40x_wrapper.sv
@@ -44,7 +44,7 @@ module cv32e40x_wrapper
 #(
   parameter NUM_MHPMCOUNTERS             =  1,
   parameter int unsigned PMA_NUM_REGIONS =  0,
-  parameter pma_region_t PMA_CFG [PMA_NUM_REGIONS-1:0] = '{default:PMA_R_DEFAULT}
+  parameter pma_region_t PMA_CFG[(PMA_NUM_REGIONS ? (PMA_NUM_REGIONS-1) : 0):0] = '{default:PMA_R_DEFAULT}
 )
 (
   // Clock and Reset
--- rtl/cv32e40x_core.sv
+++ rtl/cv32e40x_core.sv
@@ -33,7 +33,7 @@ module cv32e40x_core import cv32e40x_pkg::*;
   parameter NUM_MHPMCOUNTERS             =  1,
   parameter LIB                          =  0,
   parameter int unsigned PMA_NUM_REGIONS =  0,
-  parameter pma_region_t PMA_CFG [PMA_NUM_REGIONS-1:0] = '{default:PMA_R_DEFAULT}
+  parameter pma_region_t PMA_CFG[(PMA_NUM_REGIONS ? (PMA_NUM_REGIONS-1) : 0):0] = '{default:PMA_R_DEFAULT}
 )
 (
   // Clock and Reset
