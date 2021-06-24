ab0079178339fd78d845ce555c108cb9cc5a51ae
Oystein Knauserud <Oystein.Knauserud@silabs.com>
2021-06-24 11:03:16 +0200

    Added suppression of illegal_insn in case of deassert_we.
    Signed-off-by: Oystein Knauserud <Oystein.Knauserud@silabs.com>

--- rtl/cv32e40x_decoder.sv
+++ rtl/cv32e40x_decoder.sv
@@ -200,7 +200,6 @@ module cv32e40x_decoder import cv32e40x_pkg::*;
   assign lsu_reg_offset_o               = decoder_ctrl_mux.lsu_reg_offset;               
   assign lsu_atop_o                     = decoder_ctrl_mux.lsu_atop;                     
   assign lsu_prepost_useincr_o          = decoder_ctrl_mux.lsu_prepost_useincr;               
-  assign illegal_insn_o                 = decoder_ctrl_mux.illegal_insn;        
 
   // Suppress control signals
   assign alu_en_o             = deassert_we_i ? 1'b0        : alu_en;
@@ -211,13 +210,14 @@ module cv32e40x_decoder import cv32e40x_pkg::*;
   assign rf_we_o              = deassert_we_i ? 1'b0        : rf_we;
   assign ctrl_transfer_insn_o = deassert_we_i ? BRANCH_NONE : ctrl_transfer_insn;
 
-  // Suppress special instructions
+  // Suppress special instruction/illegal instruction bits
   assign mret_insn_o          = deassert_we_i ? 1'b0 : decoder_ctrl_mux.mret_insn;
   assign dret_insn_o          = deassert_we_i ? 1'b0 : decoder_ctrl_mux.dret_insn;
   assign ebrk_insn_o          = deassert_we_i ? 1'b0 : decoder_ctrl_mux.ebrk_insn;
   assign ecall_insn_o         = deassert_we_i ? 1'b0 : decoder_ctrl_mux.ecall_insn;
   assign wfi_insn_o           = deassert_we_i ? 1'b0 : decoder_ctrl_mux.wfi_insn;
   assign fencei_insn_o        = deassert_we_i ? 1'b0 : decoder_ctrl_mux.fencei_insn;
+  assign illegal_insn_o       = deassert_we_i ? 1'b0 : decoder_ctrl_mux.illegal_insn;
 
 
   assign ctrl_transfer_insn_raw_o = ctrl_transfer_insn;
