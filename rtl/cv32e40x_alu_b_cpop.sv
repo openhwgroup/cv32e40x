5fd2bf5a66aeb1f133b0817d06f1e8f1c0a12161
Halfdan Bechmann <halfdan.bechmann@silabs.com>
2021-07-01 17:33:08 +0200

    Fixed typo in module name
    Signed-off-by: Halfdan Bechmann <halfdan.bechmann@silabs.com>

--- rtl/cv32e40x_alu_b_cpop.sv
+++ rtl/cv32e40x_alu_b_cpop.sv
@@ -1,7 +1,33 @@
+// Copyright 2021 Silicon Labs, Inc.
+//
+// This file, and derivatives thereof are licensed under the
+// Solderpad License, Version 2.0 (the "License");
+// Use of this file means you agree to the terms and conditions
+// of the license and are in full compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://solderpad.org/licenses/SHL-2.0/
+//
+// Unless required by applicable law or agreed to in writing, software
+// and hardware implementations thereof
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESSED OR IMPLIED.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+////////////////////////////////////////////////////////////////////////////////
+// Engineers       Halfdan Bechmann  -   halfdan.bechmann@silabs.com          //
+//                                                                            //
+// Design Name:    cv32e40x_alu_b_cpop                                        //
+// Project Name:   CV32E40X                                                   //
+// Language:       SystemVerilog                                              //
+//                                                                            //
+// Description:    CPOP for Zbb extension                                     //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
 
 
-
-module alu_b_cpop
+module cv32e40x_alu_b_cpop
   (logic [31:0] operand_i,
    logic [ 5:0] result_o);
 
