abb10c617d8d2a26e0a3454717dd691a8bae4b26
Arjan Bink <Arjan.Bink@silabs.com>
2021-10-15 19:28:45 +0200

    Removed x_ prefixes and renamed interface file
    Signed-off-by: Arjan Bink <Arjan.Bink@silabs.com>

--- rtl/if_xif.sv
+++ rtl/if_xif.sv
@@ -110,101 +110,100 @@ interface if_xif import cv32e40x_pkg::*;
 
 
   // Compressed interface
-  logic               x_compressed_valid;
-  logic               x_compressed_ready;
-  x_compressed_req_t  x_compressed_req;
-  x_compressed_resp_t x_compressed_resp;
+  logic               compressed_valid;
+  logic               compressed_ready;
+  x_compressed_req_t  compressed_req;
+  x_compressed_resp_t compressed_resp;
 
   // Issue interface
-  logic               x_issue_valid;
-  logic               x_issue_ready;
-  x_issue_req_t       x_issue_req;
-  x_issue_resp_t      x_issue_resp;
+  logic               issue_valid;
+  logic               issue_ready;
+  x_issue_req_t       issue_req;
+  x_issue_resp_t      issue_resp;
 
   // Commit interface
-  logic               x_commit_valid;
-  x_commit_t          x_commit;
+  logic               commit_valid;
+  x_commit_t          commit;
 
   // Memory (request/response) interface
-  logic               x_mem_valid;
-  logic               x_mem_ready;
-  x_mem_req_t         x_mem_req;
-  x_mem_resp_t        x_mem_resp;
+  logic               mem_valid;
+  logic               mem_ready;
+  x_mem_req_t         mem_req;
+  x_mem_resp_t        mem_resp;
 
   // Memory result interface
-  logic               x_mem_result_valid;
-  x_mem_result_t      x_mem_result;
+  logic               mem_result_valid;
+  x_mem_result_t      mem_result;
 
   // Result interface
-  logic               x_result_valid;
-  logic               x_result_ready;
-  x_result_t          x_result;
-
+  logic               result_valid;
+  logic               result_ready;
+  x_result_t          result;
 
   // Port directions for host CPU
   modport cpu_compressed (
-    output x_compressed_valid,
-    input  x_compressed_ready,
-    output x_compressed_req,
-    input  x_compressed_resp
+    output compressed_valid,
+    input  compressed_ready,
+    output compressed_req,
+    input  compressed_resp
   );
   modport cpu_issue (
-    output x_issue_valid,
-    input  x_issue_ready,
-    output x_issue_req,
-    input  x_issue_resp
+    output issue_valid,
+    input  issue_ready,
+    output issue_req,
+    input  issue_resp
   );
   modport cpu_commit (
-    output x_commit_valid,
-    output x_commit
+    output commit_valid,
+    output commit
   );
   modport cpu_mem (
-    input  x_mem_valid,
-    output x_mem_ready,
-    input  x_mem_req,
-    output x_mem_resp
+    input  mem_valid,
+    output mem_ready,
+    input  mem_req,
+    output mem_resp
   );
   modport cpu_mem_result (
-    output x_mem_result_valid,
-    output x_mem_result
+    output mem_result_valid,
+    output mem_result
   );
   modport cpu_result (
-    input  x_result_valid,
-    output x_result_ready,
-    input  x_result
+    input  result_valid,
+    output result_ready,
+    input  result
   );
 
   // Port directions for extension
   modport coproc_compressed (
-    input  x_compressed_valid,
-    output x_compressed_ready,
-    input  x_compressed_req,
-    output x_compressed_resp
+    input  compressed_valid,
+    output compressed_ready,
+    input  compressed_req,
+    output compressed_resp
   );
   modport coproc_issue (
-    input  x_issue_valid,
-    output x_issue_ready,
-    input  x_issue_req,
-    output x_issue_resp
+    input  issue_valid,
+    output issue_ready,
+    input  issue_req,
+    output issue_resp
   );
   modport coproc_commit (
-    input  x_commit_valid,
-    input  x_commit
+    input  commit_valid,
+    input  commit
   );
   modport coproc_mem (
-    output x_mem_valid,
-    input  x_mem_ready,
-    output x_mem_req,
-    input  x_mem_resp
+    output mem_valid,
+    input  mem_ready,
+    output mem_req,
+    input  mem_resp
   );
   modport coproc_mem_result (
-    input  x_mem_result_valid,
-    input  x_mem_result
+    input  mem_result_valid,
+    input  mem_result
   );
   modport coproc_result (
-    output x_result_valid,
-    input  x_result_ready,
-    output x_result
+    output result_valid,
+    input  result_ready,
+    output result
   );
 
 endinterface : if_xif
