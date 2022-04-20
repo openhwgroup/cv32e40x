b756d878eed54e9677baf3f0620a6ad095aa70ee
Arjan Bink <Arjan.Bink@silabs.com>
2022-04-20 12:04:31 +0200

    Assertion fix
    Signed-off-by: Arjan Bink <Arjan.Bink@silabs.com>

--- sva/cv32e40x_rvfi_sva.sv
+++ sva/cv32e40x_rvfi_sva.sv
@@ -176,7 +176,7 @@ module cv32e40x_rvfi_sva
                      (no_debug && $stable(mtvec_addr_i)) throughout s_goto_next_rvfi_valid(rvfi_csr_dcsr_rdata[3]) |->
                      rvfi_intr.intr &&
                      (rvfi_pc_rdata == {mtvec_addr_i, NMI_MTVEC_INDEX, 2'b00}) &&
-                     ((rvfi_csr_mcause_rdata[7:0] == INT_CAUSE_LSU_LOAD_FAULT) || (rvfi_csr_mcause_rdata[7:0] == INT_CAUSE_LSU_STORE_FAULT)))
+                     ((rvfi_csr_mcause_rdata[10:0] == INT_CAUSE_LSU_LOAD_FAULT) || (rvfi_csr_mcause_rdata[10:0] == INT_CAUSE_LSU_STORE_FAULT)))
       else `uvm_error("rvfi", "dcsr.nmip not followed by rvfi_intr and NMI handler")
 
 endmodule : cv32e40x_rvfi_sva
