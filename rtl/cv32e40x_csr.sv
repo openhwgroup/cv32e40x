491aae4324a7c63b499d87abee1ee3d6975540fc
Arjan Bink <Arjan.Bink@silabs.com>
2022-06-01 18:42:18 +0200

    Removed shadow CSR related code
    Signed-off-by: Arjan Bink <Arjan.Bink@silabs.com>

--- rtl/cv32e40x_csr.sv
+++ rtl/cv32e40x_csr.sv
@@ -9,7 +9,6 @@
 
 module cv32e40x_csr #(
     parameter int unsigned    WIDTH      = 32,
-    parameter bit             SHADOWCOPY = 1'b0,
     parameter bit [WIDTH-1:0] RESETVALUE = '0
  ) (
     input  logic             clk,
@@ -17,9 +16,7 @@ module cv32e40x_csr #(
 
     input  logic [WIDTH-1:0] wr_data_i,
     input  logic             wr_en_i,
-    output logic [WIDTH-1:0] rd_data_o,
-
-    output logic             rd_error_o
+    output logic [WIDTH-1:0] rd_data_o
 );
 
   logic [WIDTH-1:0] rdata_q;
@@ -33,23 +30,5 @@ module cv32e40x_csr #(
   end
 
   assign rd_data_o = rdata_q;
-
-  if (SHADOWCOPY) begin : gen_shadow
-    logic [WIDTH-1:0] shadow_q;
-
-    always_ff @(posedge clk or negedge rst_n) begin
-      if (!rst_n) begin
-        shadow_q <= ~RESETVALUE;
-      end else if (wr_en_i) begin
-        shadow_q <= ~wr_data_i;
-      end
-    end
-
-    assign rd_error_o = rdata_q != ~shadow_q;
-
-  end else begin : gen_no_shadow
-    assign rd_error_o = 1'b0;
-  end
-
 
 endmodule
