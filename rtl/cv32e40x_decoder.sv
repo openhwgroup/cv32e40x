ae2c8cb5a1b1d43189f5da3e19a83023b5a3cec9
Oivind Ekelund <oivind.ekelund@silabs.com>
2022-11-08 11:30:11 +0100

    Check for illegal compressed instruction and illegal RV32E GPR for each sub-decoder
    Signed-off-by: Oivind Ekelund <oivind.ekelund@silabs.com>

--- rtl/cv32e40x_decoder.sv
+++ rtl/cv32e40x_decoder.sv
@@ -114,17 +114,29 @@ module cv32e40x_decoder import cv32e40x_pkg::*;
 
   logic [31:0] instr_rdata;
 
-  logic rf_illegal_addr;
+  logic dec_i_rf_illegal_addr;
+  logic dec_a_rf_illegal_addr;
+  logic dec_b_rf_illegal_addr;
+  logic dec_m_rf_illegal_addr;
   logic rf_illegal_waddr;
 
-  decoder_ctrl_t decoder_i_ctrl;
-  decoder_ctrl_t decoder_m_ctrl;
-  decoder_ctrl_t decoder_a_ctrl;
-  decoder_ctrl_t decoder_b_ctrl;
-  decoder_ctrl_t decoder_ctrl_mux_subdec;
+  decoder_ctrl_t decoder_i_ctrl, decoder_i_ctrl_int;
+  decoder_ctrl_t decoder_m_ctrl, decoder_m_ctrl_int;
+  decoder_ctrl_t decoder_a_ctrl, decoder_a_ctrl_int;
+  decoder_ctrl_t decoder_b_ctrl, decoder_b_ctrl_int;
   decoder_ctrl_t decoder_ctrl_mux;
 
   assign instr_rdata = if_id_pipe_i.instr.bus_resp.rdata;
+
+  // Check for illegal GPR address if using RV32E
+  genvar rf_rport_idx;
+  generate
+    for (rf_rport_idx = 0; rf_rport_idx < REGFILE_NUM_READ_PORTS; rf_rport_idx++) begin: gen_rf_raddr_illegal
+      assign rf_illegal_raddr_o[rf_rport_idx] = (RV32 == RV32I) ? 1'b0 : rf_raddr_i[rf_rport_idx][4];
+    end
+  endgenerate
+
+  assign rf_illegal_waddr = (RV32 == RV32I) ? 1'b0 : rf_waddr_i[4];
 
   // RV32I Base instruction set decoder
   cv32e40x_i_decoder
@@ -134,19 +146,38 @@ module cv32e40x_decoder import cv32e40x_pkg::*;
   )
   i_decoder_i
   (
-    .instr_rdata_i  ( instr_rdata    ),
-    .ctrl_fsm_i     ( ctrl_fsm_i     ),
-    .decoder_ctrl_o ( decoder_i_ctrl )
+    .instr_rdata_i  ( instr_rdata        ),
+    .ctrl_fsm_i     ( ctrl_fsm_i         ),
+    .decoder_ctrl_o ( decoder_i_ctrl_int )
   );
+
+  assign dec_i_rf_illegal_addr = (decoder_i_ctrl_int.rf_re[0] && rf_illegal_raddr_o[0]) ||
+                                 (decoder_i_ctrl_int.rf_re[1] && rf_illegal_raddr_o[1]) ||
+                                 (decoder_i_ctrl_int.rf_we    && rf_illegal_waddr);
+
+  // Take illegal compressed instruction and illegal RV32E GPR address into account
+  assign decoder_i_ctrl = (dec_i_rf_illegal_addr || if_id_pipe_i.illegal_c_insn) ?
+                          DECODER_CTRL_ILLEGAL_INSN :
+                          decoder_i_ctrl_int;
 
   generate
     if (A_EXT) begin: a_decoder
       // RV32A extension decoder
       cv32e40x_a_decoder a_decoder_i
       (
-        .instr_rdata_i  ( instr_rdata    ),
-        .decoder_ctrl_o ( decoder_a_ctrl )
+        .instr_rdata_i  ( instr_rdata        ),
+        .decoder_ctrl_o ( decoder_a_ctrl_int )
       );
+
+      assign dec_a_rf_illegal_addr = (decoder_a_ctrl_int.rf_re[0] && rf_illegal_raddr_o[0]) ||
+                                     (decoder_a_ctrl_int.rf_re[1] && rf_illegal_raddr_o[1]) ||
+                                     (decoder_a_ctrl_int.rf_we    && rf_illegal_waddr);
+
+      // Take illegal compressed instruction and illegal RV32E GPR address into account
+      assign decoder_a_ctrl = (dec_a_rf_illegal_addr || if_id_pipe_i.illegal_c_insn) ?
+                              DECODER_CTRL_ILLEGAL_INSN :
+                              decoder_a_ctrl_int;
+
     end else begin: no_a_decoder
       assign decoder_a_ctrl = DECODER_CTRL_ILLEGAL_INSN;
     end
@@ -160,8 +191,18 @@ module cv32e40x_decoder import cv32e40x_pkg::*;
       b_decoder_i
       (
         .instr_rdata_i      ( instr_rdata                        ),
-        .decoder_ctrl_o     ( decoder_b_ctrl                     )
+        .decoder_ctrl_o     ( decoder_b_ctrl_int                 )
       );
+
+      assign dec_b_rf_illegal_addr = (decoder_b_ctrl_int.rf_re[0] && rf_illegal_raddr_o[0]) ||
+                                     (decoder_b_ctrl_int.rf_re[1] && rf_illegal_raddr_o[1]) ||
+                                     (decoder_b_ctrl_int.rf_we    && rf_illegal_waddr);
+
+      // Take illegal compressed instruction and illegal RV32E GPR address into account
+      assign decoder_b_ctrl = (dec_b_rf_illegal_addr || if_id_pipe_i.illegal_c_insn) ?
+                              DECODER_CTRL_ILLEGAL_INSN :
+                              decoder_b_ctrl_int;
+
     end else begin: no_b_decoder
       assign decoder_b_ctrl = DECODER_CTRL_ILLEGAL_INSN;
     end
@@ -174,9 +215,19 @@ module cv32e40x_decoder import cv32e40x_pkg::*;
           )
       m_decoder_i
       (
-       .instr_rdata_i  ( instr_rdata    ),
-       .decoder_ctrl_o ( decoder_m_ctrl )
+       .instr_rdata_i  ( instr_rdata        ),
+       .decoder_ctrl_o ( decoder_m_ctrl_int )
       );
+
+      assign dec_m_rf_illegal_addr = (decoder_m_ctrl_int.rf_re[0] && rf_illegal_raddr_o[0]) ||
+                                     (decoder_m_ctrl_int.rf_re[1] && rf_illegal_raddr_o[1]) ||
+                                     (decoder_m_ctrl_int.rf_we    && rf_illegal_waddr);
+
+      // Take illegal compressed instruction and illegal RV32E GPR address into account
+      assign decoder_m_ctrl = (dec_m_rf_illegal_addr || if_id_pipe_i.illegal_c_insn) ?
+                              DECODER_CTRL_ILLEGAL_INSN :
+                              decoder_m_ctrl_int;
+
     end else begin: no_m_decoder
       assign decoder_m_ctrl = DECODER_CTRL_ILLEGAL_INSN;
     end
@@ -187,37 +238,12 @@ module cv32e40x_decoder import cv32e40x_pkg::*;
   always_comb
   begin
     unique case (1'b1)
-      !decoder_m_ctrl.illegal_insn : decoder_ctrl_mux_subdec = decoder_m_ctrl; // M decoder got a match
-      !decoder_a_ctrl.illegal_insn : decoder_ctrl_mux_subdec = decoder_a_ctrl; // A decoder got a match
-      !decoder_i_ctrl.illegal_insn : decoder_ctrl_mux_subdec = decoder_i_ctrl; // I decoder got a match
-      !decoder_b_ctrl.illegal_insn : decoder_ctrl_mux_subdec = decoder_b_ctrl; // B decoder got a match
-      default                      : decoder_ctrl_mux_subdec = DECODER_CTRL_ILLEGAL_INSN; // No match from decoders, illegal instruction
+      !decoder_m_ctrl.illegal_insn : decoder_ctrl_mux = decoder_m_ctrl; // M decoder got a match
+      !decoder_a_ctrl.illegal_insn : decoder_ctrl_mux = decoder_a_ctrl; // A decoder got a match
+      !decoder_i_ctrl.illegal_insn : decoder_ctrl_mux = decoder_i_ctrl; // I decoder got a match
+      !decoder_b_ctrl.illegal_insn : decoder_ctrl_mux = decoder_b_ctrl; // B decoder got a match
+      default                      : decoder_ctrl_mux = DECODER_CTRL_ILLEGAL_INSN; // No match from decoders, illegal instruction
     endcase
-  end
-
-  // Check for illegal GPR address if using RV32E
-  genvar rf_rport_idx;
-  generate
-    for (rf_rport_idx = 0; rf_rport_idx < REGFILE_NUM_READ_PORTS; rf_rport_idx++) begin: gen_rf_raddr_illegal
-      assign rf_illegal_raddr_o[rf_rport_idx] = (RV32 == RV32I) ? 1'b0 : rf_raddr_i[rf_rport_idx][4];
-    end
-  endgenerate
-
-  assign rf_illegal_waddr = (RV32 == RV32I) ? 1'b0 : rf_waddr_i[4];
-
-  assign rf_illegal_addr = (decoder_ctrl_mux_subdec.rf_re[0] && rf_illegal_raddr_o[0]) ||
-                           (decoder_ctrl_mux_subdec.rf_re[1] && rf_illegal_raddr_o[1]) ||
-                           (decoder_ctrl_mux_subdec.rf_we    && rf_illegal_waddr);
-
-
-  // Take illegal compressed instruction and illegal RV32E GPR address into account
-  always_comb begin
-    if (if_id_pipe_i.illegal_c_insn || rf_illegal_addr) begin
-      decoder_ctrl_mux = DECODER_CTRL_ILLEGAL_INSN;
-    end
-    else begin
-      decoder_ctrl_mux = decoder_ctrl_mux_subdec;
-    end
   end
 
   assign alu_en             = decoder_ctrl_mux.alu_en;
