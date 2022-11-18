90bcf5fe29b49b4fe05b266faf0b66cb40a85b91
Oystein Knauserud <Oystein.Knauserud@silabs.com>
2022-11-18 13:54:05 +0100

    Turned on ZC_EXT.
    ZC_EXT localpram in toplevel is now 1, and sequencer assertions bound by default in the wrapper
    
    Signed-off-by: Oystein Knauserud <Oystein.Knauserud@silabs.com>

--- rtl/cv32e40x_core.sv
+++ rtl/cv32e40x_core.sv
@@ -137,8 +137,7 @@ module cv32e40x_core import cv32e40x_pkg::*;
   localparam int unsigned REGFILE_NUM_READ_PORTS = X_EXT ? X_NUM_RS : 2;
 
   // Zc is always present
-  // todo: turn on when Zc is ready for core-v-verif
-  localparam bit ZC_EXT = 0;
+  localparam bit ZC_EXT = 1;
 
   // Determine alignedness of mtvt
   // mtvt[31:N] holds mtvt table entry
