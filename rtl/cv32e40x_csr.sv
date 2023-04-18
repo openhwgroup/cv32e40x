7ba38680c6853f9306430f8b020f3bac33aa4fdb
Oystein Knauserud <Oystein.Knauserud@silabs.com>
2023-04-18 14:00:49 +0200

    Proposed fix for issue #766.
    - Introducing MASK and RESET_VAL for all CSRs.
    - Introducing function csr_nextvalue to take write data, reset value and mask into account.
    
    This PR addresses five different CSRs, the rest will be done once this PR is approved.
    
    SEC clean.
    
    Signed-off-by: Oystein Knauserud <Oystein.Knauserud@silabs.com>

--- rtl/cv32e40x_csr.sv
+++ rtl/cv32e40x_csr.sv
@@ -9,7 +9,8 @@
 
 module cv32e40x_csr #(
     parameter int unsigned    WIDTH      = 32,
-    parameter bit [WIDTH-1:0] RESETVALUE = '0
+    parameter bit [WIDTH-1:0] RESETVALUE = '0,
+    parameter bit [WIDTH-1:0] MASK       = '1
  ) (
     input  logic             clk,
     input  logic             rst_n,
@@ -21,14 +22,26 @@ module cv32e40x_csr #(
 
   logic [WIDTH-1:0] rdata_q;
 
-  always_ff @(posedge clk or negedge rst_n) begin
-    if (!rst_n) begin
-      rdata_q <= RESETVALUE;
-    end else if (wr_en_i) begin
-      rdata_q <= wr_data_i;
-    end
-  end
+  generate
+    for (genvar i = 0; i < WIDTH; i++) begin : gen_csr
+      if (MASK[i]) begin : gen_unmasked
+        // Bits with mask set are actual flipflops
+        always_ff @(posedge clk or negedge rst_n) begin
+          if (!rst_n) begin
+            rdata_q[i] <= RESETVALUE[i];
+          end else if (wr_en_i) begin
+            rdata_q[i] <= wr_data_i[i];
+          end
+        end
+      end else begin : gen_masked
+        // Bits with mask cleared are tied off to the reset value
+        assign rdata_q[i] = RESETVALUE[i];
+      end
+    end // for
+  endgenerate
 
   assign rd_data_o = rdata_q;
+
+
 
 endmodule
