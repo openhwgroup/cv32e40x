4965a65b5b1674d1973dad0782cdb34afbf249cd
Oivind Ekelund <oivind.ekelund@silabs.com>
2023-08-31 14:44:55 +0200

    Fix todos. Update a_kill_if and a_kill_id.
    Signed-off-by: Oivind Ekelund <oivind.ekelund@silabs.com>

--- sva/cv32e40x_controller_fsm_sva.sv
+++ sva/cv32e40x_controller_fsm_sva.sv
@@ -91,6 +91,7 @@ module cv32e40x_controller_fsm_sva
   input logic           first_op_ex_i,
   input logic           last_op_id_i,
   input logic           ex_ready_i,
+  input logic           id_ready_i,
   input logic           sequence_interruptible,
   input logic           sequence_in_progress_wb,
   input logic           id_stage_haltable,
@@ -191,20 +192,35 @@ module cv32e40x_controller_fsm_sva
       else `uvm_error("controller", "Interrupt taken while oustanding transactions are pending")
 
   // Ensure <stage>.instr_valid is zero following a kill_<prev_stage>
- /* TODO:OK:low Failing when bubble is inserted in ID (id_ready_o==0) when WFI is in EX.
-            Will investigate how to solve. Agreed that this assertion is maybe too strict. We only need to guarantee that if a stage is killed, that the instruction in that stage never reaches the following stage with instr_valid = 1 (it doesn't need instr_valid of the next stage 0 in the following cycle.
+
+  // Support logic to indicate that IF has been killed, and not yet received a new instruction from the prefetcher
+  logic kill_if_no_prefetch_valid;
+  always_ff @(posedge clk, negedge rst_n) begin
+    if(!rst_n) begin
+      kill_if_no_prefetch_valid <= 1'b0;
+    end
+    else begin
+      if (ctrl_fsm_o.kill_if) begin
+        kill_if_no_prefetch_valid <= 1'b1;
+      end
+      else if (prefetch_valid_if_i) begin
+        kill_if_no_prefetch_valid <= 1'b0;
+      end
+    end
+  end
+
+  // If IF is killed and ID is ready before a new instruction arrives, if_id_pipe_i.instr_valid must be 0
   a_kill_if :
   assert property (@(posedge clk) disable iff (!rst_n)
-                    (ctrl_fsm_o.kill_if) |=> (if_id_pipe_i.instr_valid == 1'b0) )
-    else `uvm_error("controller", "if_id_pipe.instr_valid not zero after kill_if")
-*/
-/* TODO:OK:low Failing when a DIV instruction is being executed
-           Causes ex_ready to be 0. Will be fixed then divider is interruptable
+                    kill_if_no_prefetch_valid && $past(id_ready_i) |-> (if_id_pipe_i.instr_valid == 1'b0) )
+    else `uvm_error("controller", "if_id_pipe_i.instr_valid not zero after kill_if")
+
+  // If ID is killed, id_ex_pipe_i.instr_valid must be 0 following the next ex_ready_i
   a_kill_id :
   assert property (@(posedge clk) disable iff (!rst_n)
-                    (ctrl_fsm_o.kill_id) |=> (id_ex_pipe_i.instr_valid == 1'b0) )
+                    ctrl_fsm_o.kill_id ##0 ex_ready_i[->1] |=> (id_ex_pipe_i.instr_valid == 1'b0) )
     else `uvm_error("controller", "id_ex_pipe.instr_valid not zero after kill_id")
-*/
+
   a_kill_ex :
   assert property (@(posedge clk) disable iff (!rst_n)
                     (ctrl_fsm_o.kill_ex && !ctrl_fsm_o.halt_wb) |=> (ex_wb_pipe_i.instr_valid == 1'b0) )
