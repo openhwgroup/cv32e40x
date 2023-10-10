acc2cac1bdb5fce4b0e42324d94e3f36466bbbcf
Oystein Knauserud <Oystein.Knauserud@silabs.com>
2023-10-10 11:57:29 +0200

    Using priv_lvl_q for priv_lvl_rdata.
    Signed-off-by: Oystein Knauserud <Oystein.Knauserud@silabs.com>

--- rtl/cv32e40x_cs_registers.sv
+++ rtl/cv32e40x_cs_registers.sv
@@ -1520,7 +1520,7 @@ module cv32e40x_cs_registers import cv32e40x_pkg::*;
   assign mconfigptr_rdata   = 32'h0;
 
   // Only machine mode is supported
-  assign priv_lvl_rdata     = PRIV_LVL_M;
+  assign priv_lvl_rdata     = priv_lvl_q;
   assign priv_lvl_q         = PRIV_LVL_M;
   assign priv_lvl_lsu_o     = PRIV_LVL_M;
   assign priv_lvl_if_ctrl_o.priv_lvl     = PRIV_LVL_M;
@@ -1940,6 +1940,6 @@ module cv32e40x_cs_registers import cv32e40x_pkg::*;
   assign unused_signals = mstatush_we | misa_we | mip_we | mvendorid_we |
     marchid_we | mimpid_we | mhartid_we | mconfigptr_we | mtval_we | (|mnxti_n) | mscratchcswl_we |
     (|mscratchcswl_rdata) | (|mscratchcswl_n) |mscratchcswl_in_wb | mnxti_in_wb |
-    (|mtval_n) | (|mconfigptr_n) | (|mhartid_n) | (|mimpid_n) | (|marchid_n) | (|mvendorid_n) | (|mip_n) | (|misa_n) | (|mstatush_n) | (|priv_lvl_q);
+    (|mtval_n) | (|mconfigptr_n) | (|mhartid_n) | (|mimpid_n) | (|marchid_n) | (|mvendorid_n) | (|mip_n) | (|misa_n) | (|mstatush_n);
 
 endmodule
